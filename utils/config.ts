7470899179641c71299097a81143cd019eb61055
fitzypop <fitzypop@pm.me>
2022-09-14 23:15:03 -0600

    remove unneeded config call

--- utils/config.ts
+++ utils/config.ts
@@ -1,6 +1,2 @@
-import { config } from "dotenv";
-
-await config({ export: true });
-
 export const supabase_url = Deno.env.get("PUBLIC_SUPABASE_URL") || "";
 export const supabase_key = Deno.env.get("PUBLIC_SUPABASE_API_KEY") || "";
