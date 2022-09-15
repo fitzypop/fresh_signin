200755cb5e7bc63e93138dd0d7b3819b3df12758
fitzypop <fitzypop@pm.me>
2022-09-14 20:07:36 -0600

    current 'final' fix for env variables

---
+++ utils/config.ts
@@ -0,0 +1,10 @@
+import { config as get_envs } from "https://deno.land/std@0.155.0/dotenv/mod.ts";
+
+// HACK: Deno Deploy automagically adds env variables
+// HACK: But local deno does not, so we need to use std/dotenv
+// HACK: DON"T USE deno.land/x/dotenv <- Deno Deploy does not support full file system api
+const env = await get_envs();
+const [url_key, api_key] = ["PUBLIC_SUPABASE_URL", "PUBLIC_SUPABASE_API_KEY"];
+
+export const supabase_url = env[url_key] || Deno.env.get(url_key) || "";
+export const supabase_key = env[api_key] || Deno.env.get(api_key) || "";
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,18 +1,9 @@
-import { createClient } from "supabase";
-import { config } from "https://deno.land/std@0.155.0/dotenv/mod.ts";
+import { createClient, SupabaseClientOptions } from "supabase";
+import { supabase_key, supabase_url } from "@/utils/config.ts";
 
-// HACK: Deno Deploy automagically adds env variables
-// HACK: But local deno does not, so we need to use std/dotenv
-// HACK: DON"T USE deno.land/x/dotenv <- Deno Deploy does not support full file system api
-const env = await config();
-if (!env) {
-  console.log("empty env", env);
-}
-console.log(env);
-const url = env.PUBLIC_SUPABASE_URL || Deno.env.get("PUBLIC_SUPABASE_URL") ||
-  "";
-const key = env.PUBLIC_SUPABASE_API_KEY ||
-  Deno.env.get("PUBLIC_SUPABASE_API_KEY") || "";
+const options: SupabaseClientOptions = {
+  autoRefreshToken: true,
+};
 
-const supabase = createClient(url, key, { autoRefreshToken: true });
-export default supabase;
+const supaClient = createClient(supabase_url, supabase_key, options);
+export default supaClient;
