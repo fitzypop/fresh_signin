9918946fcdf15684d32cdd4c81f43c37d25aac1c
fitzypop <fitzypop@pm.me>
2022-09-14 19:37:59 -0600

    more env debugging

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,8 +3,10 @@ import { config } from "https://deno.land/std@0.155.0/dotenv/mod.ts";
 
 const env = await config();
 console.log(env);
-const url = env.PUBLIC_SUPABASE_URL || "";
-const key = env.PUBLIC_SUPABASE_API_KEY || "";
+const url = env.PUBLIC_SUPABASE_URL || Deno.env.get("PUBLIC_SUPABASE_URL") ||
+  "";
+const key = env.PUBLIC_SUPABASE_API_KEY ||
+  Deno.env.get("PUBLIC_SUPABASE_API_KEY") || "";
 
 const supabase = createClient(url, key, { autoRefreshToken: true });
 export default supabase;
