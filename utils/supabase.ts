16ab6e4074e8ce8e1a43ac537832bdf6082d6f9d
fitzypop <fitzypop@pm.me>
2022-09-14 19:34:49 -0600

    debugging deploy

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,6 +2,7 @@ import { createClient } from "supabase";
 import { config } from "https://deno.land/std@0.155.0/dotenv/mod.ts";
 
 const env = await config();
+console.log(env);
 const url = env.PUBLIC_SUPABASE_URL || "";
 const key = env.PUBLIC_SUPABASE_API_KEY || "";
 
