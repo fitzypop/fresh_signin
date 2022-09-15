a0263c41a20c8a01d90b5579217d7f204b1f3b4a
fitzypop <fitzypop@pm.me>
2022-09-15 12:42:00 -0600

    add redirect

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,15 +3,16 @@ import Page from "@/components/Page.tsx";
 import { HandlerContext, Handlers, PageProps } from "$fresh/server.ts";
 import Counter from "@/islands/Counter.tsx";
 import supaClient from "@/utils/supabase.ts";
+import { base_url } from "@/utils/config.ts";
 
-// export const handler: Handlers = {
-//   async GET(req: Request, ctx: HandlerContext) {
-//     if (supaClient.auth.session()) {
-//       return Response.redirect(new URL(`${req.url}dash`));
-//     }
-//     return await ctx.render();
-//   },
-// };
+export const handler: Handlers = {
+  async GET(_req: Request, ctx: HandlerContext) {
+    if (supaClient.auth.session()) {
+      return Response.redirect(new URL(`${base_url}/dash`));
+    }
+    return await ctx.render();
+  },
+};
 
 export default function Index(_props: PageProps) {
   return (
